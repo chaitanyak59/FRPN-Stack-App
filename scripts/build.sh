162564173797dee0be03d35226676bce5ee648e9
Chaitanya Kumar <chaitanya@Chaitanyas-MacBook-Air.local>
2020-04-10 20:02:35 +0530

    Finaleee

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,7 +7,7 @@ set -e
 
     #Migrations Create Tables,Inits..
     echo "Running Migrations"
-    cd server && PGSSLMODE=require npm run migrate && cd ..
+    cd server && PGSSLMODE=require yarn migrate-up && cd ..
     if [ $? -ne 0 ]; then
         echo "Migration failed"
         exit 1
